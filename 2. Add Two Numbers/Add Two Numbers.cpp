cef9581c58fabe7156e8ba8c2b28183bf652440c
JackieTao123 <tzy_china@msn.cn>
2016-07-12 13:11:30 +0800

    changed by JackieTao

---
+++ 2. Add Two Numbers/Add Two Numbers.cpp
@@ -0,0 +1,32 @@
+#include "Add Two Numbersr.h"
+#include <stack>
+using std::stack;
+int main()
+{
+	ListNode *L1 = new ListNode(1);
+	//L1->next = new ListNode(8);
+	/*
+	L1->next->next = new ListNode(3);*/
+	//L1->next->next->next = nullptr;
+	//L1->next->next = nullptr;
+
+	ListNode *L2 = new ListNode(9);
+	L2->next = new ListNode(9);
+	//L2->next->next = new ListNode(4);
+	//L2->next->next->next = nullptr;
+	Solution s;
+	ListNode *result = s.addTwoNumbers(L1, L2);
+	stack<int> temp;
+	while (result!=nullptr)
+	{
+		temp.push(result->val);
+		result = result->next;
+	}
+	while (!temp.empty())
+	{
+		std::cout << temp.top() << std::endl;
+		temp.pop();
+	}
+	system("pause");
+	return 0;
+}
