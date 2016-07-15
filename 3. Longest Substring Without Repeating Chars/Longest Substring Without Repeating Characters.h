66dc84f9c77283e79f0c953e5628dc1ba74ea53b
JackieTao123 <tzy_china@msn.cn>
2016-07-15 19:05:59 +0800

    modified this header to implement the algorithm.

--- 3. Longest Substring Without Repeating Chars/Longest Substring Without Repeating Characters.h
+++ 3. Longest Substring Without Repeating Chars/Longest Substring Without Repeating Characters.h
@@ -12,39 +12,83 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 */
 #include <iostream>
 #include <string>
-
+#include <unordered_map>
+#include <algorithm>
+using std::unordered_map;
 using std::string;
-class Solution {
-public:
-	int lengthOfLongestSubstring(string s) {
-		if (s.empty())
-		{
-			return 0;
-		}
-		char *p_start = &s[0];
-		int len = 0;
-		while (*p_start != '\0')
-		{
-			int temp_len = 0;
-			char *q_scan = p_start;
-			//q_scan = p_start + 1;
-			while (*q_scan != '\0')
-			{
-				++temp_len;
-				if (*(q_scan+1)!= *p_start)
-					q_scan += 1;
-				else
-					break;
-			}
-			if (len < temp_len)
-			{
-				len = temp_len;
-			}
-			if (*(p_start) == '\0')
-				return len;
-				p_start += 1;
-		}
-		return len;
 
-	}
+/*
+BELOW CODE IS WRONG,JUST CAN CALCULATE SOME SIMPLE CASES.
+=====================================================================================
+//
+//class Solution {
+//public:
+//	int lengthOfLongestSubstring(string s) {
+//		if (s.empty())
+//		{
+//			return 0;
+//		}
+//		char *p_start = &s[0];
+//		int len = 0;
+//		while (*p_start != '\0')
+//		{
+//			int temp_len = 0;
+//			char *q_scan = p_start;
+//			//q_scan = p_start + 1;
+//			while (*q_scan != '\0')
+//			{
+//				++temp_len;
+//				if (*(q_scan+1)!= *p_start)
+//					q_scan += 1;
+//				else
+//					break;
+//			}
+//			if (len < temp_len)
+//			{
+//				len = temp_len;
+//			}
+//			if (*(p_start) == '\0')
+//				return len;
+//				p_start += 1;
+//		}
+//		return len;
+//
+//	}
+//};
+=====================================================================================
+*/
+class Solution{
+	public:
+		int lengthOfLongestSubstring(string s){
+			//use unordered_map to implement a simple hash table.
+			unordered_map<char, bool> mapping;
+			int len = 0;
+			//use p_start to scan the string.
+			char *p_start = &s[0];
+
+			int temp_len = 0;
+			while (*p_start != '\0')
+			{
+				if (!mapping[*p_start])
+				{
+					++temp_len;
+					mapping[*p_start] = true;
+					++p_start;
+					if (*p_start == '\0')
+						return std::max(len, temp_len);
+				}
+				else
+				{
+					len = std::max(len, temp_len);
+					mapping.clear();
+					//mapping[*p_start] = true;
+					temp_len = 0;
+					//++p_start;
+					//break;
+				}
+				
+			}
+			return len;
+
+		}
 };
