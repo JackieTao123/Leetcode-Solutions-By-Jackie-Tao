f411ba25373c9cf7cfd97cc7b0036e7d7ca14b70
JackieTao123 <tzy_china@msn.cn>
2016-07-15 14:08:45 +0800

    Use double while loop to implement

---
+++ 3. Longest Substring Without Repeating Chars/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,10 @@
+#include "Longest Substring Without Repeating Characters.h"
+
+int main()
+{
+	std::string test("abcabcbb");
+	Solution s;
+	std::cout << s.lengthOfLongestSubstring(test) << std::endl;
+	system("pause");
+	return 0;
+}
--- 3. Longest Substring Without Repeating Chars/Longest Substring Without Repeating Characters.h
+++ 3. Longest Substring Without Repeating Chars/Longest Substring Without Repeating Characters.h
@@ -9,14 +9,42 @@ Given "bbbbb", the answer is "b", with the length of 1.
 
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-Subscribe to see which companies asked this question
-
 */
 #include <iostream>
+#include <string>
+
 using std::string;
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
+		if (s.empty())
+		{
+			return 0;
+		}
+		char *p_start = &s[0];
+		int len = 0;
+		while (*p_start != '\0')
+		{
+			int temp_len = 0;
+			char *q_scan = p_start;
+			//q_scan = p_start + 1;
+			while (*q_scan != '\0')
+			{
+				++temp_len;
+				if (*(q_scan+1)!= *p_start)
+					q_scan += 1;
+				else
+					break;
+			}
+			if (len < temp_len)
+			{
+				len = temp_len;
+			}
+			if (*(p_start) == '\0')
+				return len;
+				p_start += 1;
+		}
+		return len;
 
 	}
 };
