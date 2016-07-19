63a873e7a42c497a74cb8e09f6b2444fe2057dfc
JackieTao123 <tzy_china@msn.cn>
2016-07-19 19:37:24 +0800

    Add new Sliding Window meethod using SET

--- 3. Longest Substring Without Repeating Chars/Sliding_Window.h
+++ 3. Longest Substring Without Repeating Chars/Sliding_Window.h
@@ -10,6 +10,9 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 */
+
+//This method use set to tell whether a charactrer is repeated or not...
+
 #include <iostream>
 #include <string>
 #include <set>
